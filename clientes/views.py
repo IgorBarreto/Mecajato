de9c3ab7f365a199784255876e225e0429bde1fc
Igor Barreto <igorbarreto1993@hotmail.com>
2022-11-01 23:34:49 -0300

    Feat: Criando a tela de atualização dos clientes e buscando seus dados do banco

--- clientes/views.py
+++ clientes/views.py
@@ -1,12 +1,15 @@
 import re
+import json
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.core import serializers
 from .models import Cliente, Carro
 
 # Create your views here.
 def clientes(request: HttpRequest):
     if request.method == "GET":
-        return render(request, "clientes.html")
+        clienteslist = Cliente.objects.all()
+        return render(request, "clientes.html", {"clientes": clienteslist})
     elif request.method == "POST":
         nome = request.POST.get("nome")
         sobrenome = request.POST.get("sobrenome")
@@ -62,3 +65,12 @@ def clientes(request: HttpRequest):
             )
             carro.save()
         return HttpResponse("teste")
+
+
+def att_cliente(request: HttpRequest):
+    id_cliente = request.POST.get("id_cliente")
+    cliente = Cliente.objects.filter(id=id_cliente)
+    clietes_json = json.loads(serializers.serialize("json", cliente))[0][
+        "fields"
+    ]
+    return JsonResponse(clietes_json)
--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -17,3 +17,49 @@ function add_carro() {
 
     container.innerHTML += html
 }
+
+function exibir_form(tipo) {
+    add_cliente = document.getElementById('adicionar-cliente');
+    att_clientes = document.getElementById('att_clientes');
+
+    if (tipo === "1") {
+        att_clientes.style.display = 'none';
+        add_cliente.style.display = 'block';
+    }
+    else if (tipo === "2") {
+        add_cliente.style.display = 'none';
+        att_clientes.style.display = 'block';
+    }
+}
+
+function dados_cliente() {
+    cliente = document.getElementById('cliente-select');
+    console.log(cliente.value)
+    csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
+    id_cliente = cliente.value
+    data = new FormData()
+    data.append('id_cliente', id_cliente)
+    fetch("atualiza_cliente", {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrf_token
+        },
+        body: data
+    }).then(function (result) {
+        return result.json()
+    }).then(function (data) {
+        document.getElementById('form-att-cliente').style.display = 'block'
+
+        nome = document.getElementById('nome')
+        nome.value = data['nome']
+
+        sobrenome = document.getElementById('sobrenome')
+        sobrenome.value = data['sobrenome']
+
+        cpf = document.getElementById('cpf')
+        cpf.value = data['cpf']
+
+        email = document.getElementById('email')
+        email.value = data['email']
+    })
+}
